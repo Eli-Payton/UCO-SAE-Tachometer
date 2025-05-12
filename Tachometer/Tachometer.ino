71a6fb40e3e54e847ba973aeb8f46f61c50b9d08
Eli Payton <eli@paytonclan.com>
2025-05-12 16:23:59 -0500

    Documentation update

--- Tachometer/Tachometer.ino
+++ Tachometer/Tachometer.ino
@@ -1,46 +1,46 @@
- const int number_of_LEDs = 10;       //Must Set these 3!!!
- const int starting_pin = 2;
- const int rpm_range = 10000;
- const int VRy = A0;  // Y-axis
- int x1 = 0;
+ const int number_of_LEDs = 10;        //Must Set these variables!!!
+ const int starting_pin = 2;           //First digital pin with LED
+ const int rpm_range = 10000;          //Max RPMs (or whatever other metric)
+ const int VRy = A0;                   //Digital pin number for the Y-input of the controller
+ int x1 = 0;                           //X1 is used as the global RPM value
 
 void setup() {
 
    Serial.begin(9600);
-   Serial.println("Hello, world!");
+   Serial.println("Starting Program");
    delay(1000);
 
 }
 
- void loop() {
-  x1 = set_x(x1);
+ void loop() {                                          //Loop cycles through 5 times per second, uses set_x() to update x1, then prints out the values of the joystick input and x1, then RPM_check() is called
+  x1 = set_x(x1);                                       //to light up the appropriate LEDs
   Serial.println(analogRead(VRy));
   Serial.println(x1);
   RPM_check(x1);
   delay(200);
  }
 
-void RPM_check(int rpm) {
+void RPM_check(int rpm) {                                   //This function calculates how many LEDs should be lit up and then calls the brighten_num function to brighten those LEDs
   int rpm_threshold_size = (rpm_range / number_of_LEDs);
 
   for (int i = 0; i < number_of_LEDs; i++){
-    if (rpm > (i * rpm_threshold_size) ){       //Swapping this greater than
+    if (rpm > (i * rpm_threshold_size) ){       
       brighten_num(i+1);
     }
   }
 }
 
-int set_x (int x) {
-  if (Serial.available() > 0) {
-     String input = Serial.readStringUntil('\n'); // Reads until Enter is pressed
+int set_x (int x) {                                         //This function updates the value that the tachometer is displaying. If the user inputs a number to the keyboard, we return that number.
+  if (Serial.available() > 0) {                             //If the user doesn't enter a new number, then we call the joystick_accelerator function to check if the user has made an input with the joystick.
+     String input = Serial.readStringUntil('\n'); // Reads until Enter key is pressed
      return input.toInt();
-}
+  }
   else {
     return joystick_accelerator(x);
   }
 }
 
-void brighten_num(int x){                         //digital write is not working with current configuration
+void brighten_num(int x){                         //This functions takes the number of LEDs it needs to light up as an input and then brightens them while dimming all other LEDs
   for (int i = 0; i < number_of_LEDs; i++) {
       if (i < x) {
         digitalWrite(starting_pin + i, HIGH);
@@ -50,9 +50,9 @@ void brighten_num(int x){                         //digital write is not working
     }
 }
 
-int joystick_accelerator(int x) {
-  int yValue = analogRead(VRy);
-  if (x < 0){
+int joystick_accelerator(int x) {                 //This function takes the original RPM as an input and checks if the user has moved the joystick up or down. Depending on the user's input to the joystick       
+  int yValue = analogRead(VRy);                   //the function will either return a larger, smaller, or the same RPM value. The second if statement functions as a deazone so that insignificant inputs are
+  if (x < 0){                                     //not considered user inputs.
     x = 0;
   }
   if(yValue > 580 || yValue < 450){
